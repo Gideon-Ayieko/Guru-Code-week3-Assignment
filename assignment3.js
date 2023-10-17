f97825b2f81332db1c090cd35eadad9618a14798
Gideon-Ayieko <goaonduru@gmail.com>
2023-10-17 13:20:32 +0300

    Second Commit

--- assignment3.js
+++ assignment3.js
@@ -1,7 +1,54 @@
 //TASK: WEEK 3 ASSIGNMENT
 //AUTHOR: GIDEON ONDURU AYIEKO
 
-//Convert the value to a number
+//QUESTION 1:Convert the value to a number
 const number = "45";
-number=Number(number);
-console.log(number);
+console.log(Number(number));
+
+//QUESTION 2: 
+const printSum = "20" + 40;  //let the result print 60. //*hint: 60.*
+console.log(+printSum);
+
+const sentence = "I am " + (23 + 4) +" " + "years old"; //*hint: “I am 27 years old”*
+console.log(sentence);
+
+//QUESTION 3: Copy the code below and paste on your IDE, find the bug in the code below and fix it.
+let myVaue = "May";
+myVaue = 86;
+
+//QUESTION 4: Declare three camel case variables of your first name, last name, guru coding course, 
+//test score and initialize them with the right values. 
+
+let firstName = "Gideon";
+let lastName = "Ayieko";
+let guruCodingCourse = "JavaScript";
+let testScore = 99;
+
+//QUESTION 5: Using a switch statement or If-else condition. The average scores of a JavaScript test is:
+let averageScore=55;
+
+if (averageScore > 0 && averageScore < 100)
+{
+    if(averageScore < 30)
+        console.log("F");
+    else if (averageScore > 30 && averageScore <= 40)
+        console.log("E");
+    else if (averageScore > 40 && averageScore <= 50)
+        console.log("D");
+    else if (averageScore > 50 && averageScore <= 60)
+        console.log("C");
+    else if(averageScore > 60 && averageScore <= 70)
+        console.log("B");
+    else if (averageScore > 70)
+        console.log("A");
+}
+else
+    console.log("The test score is out of range!");
+
+
+
+
+
+
+
+
